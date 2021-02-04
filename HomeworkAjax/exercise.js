bd709e27b1877f00dfa64046769598c34dac689e
Sashkasara <sarafinovska.sashka@yahoo.com>
2021-02-04 21:06:04 +0100

    ajax

---
+++ HomeworkAjax/exercise.js
@@ -0,0 +1,33 @@
+document.getElementById("button").addEventListener("click", function() {
+    $.ajax({
+        url:"https://jsonplaceholder.typicode.com/photos",
+        success: function(result) {
+            let array = [];
+            for( let i=0; i < 100; i++){
+                array.push(result[i])
+            }
+            if (array !== undefined && array.length > 0) {
+                $("#output").append (
+                    `
+                        <ul>
+                            ${array.map((photo) => {
+
+                                return (
+                                    `
+                                        <li> ${photo.id}</li> < /br>
+                                        <li> ${photo.title} </li> < /br>
+                                        <li> ${photo.url} </li> < /br>
+                                        <li> ${photo.title} </li> < /br>
+                                    `
+                                )
+                            })}
+                        </ul>
+                    `
+                )
+            }
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    })
+}, false)
